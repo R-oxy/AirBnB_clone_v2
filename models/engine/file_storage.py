dc9842a329cf99456fb955897f1be46350379b72
R-oxy <aronmangati@gmail.com>
2023-09-19 09:58:51 -0700

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,14 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            filtered_objects = {}
+            for key, obj in FileStorage.__objects.items():
+                if obj.__class__ == cls:
+                    filtered_objects[key] = obj
+            return filtered_objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
